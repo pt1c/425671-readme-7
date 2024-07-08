dbe238ac39dcee6b4ef9c8300e06158726cb565d
pt1c <kirill@savinov.me>
2024-07-08 13:42:19 +0300

    + compare password

--- project/libs/account/blog-user/src/blog-user-module/blog-user.entity.ts
+++ project/libs/account/blog-user/src/blog-user-module/blog-user.entity.ts
@@ -1,6 +1,6 @@
 import { Entity } from '@project/shared/core';
 import { StorableEntity, AuthUser} from '@project/shared/core';
-import { genSalt, hash } from 'bcrypt';
+import { compare, genSalt, hash } from 'bcrypt';
 import { SALT_ROUNDS } from './blog-user.constant';
 
 export class BlogUserEntity extends Entity implements StorableEntity<AuthUser> {
@@ -42,5 +42,9 @@ export class BlogUserEntity extends Entity implements StorableEntity<AuthUser> {
     const salt = await genSalt(SALT_ROUNDS);
     this.passwordHash = await hash(password, salt);
     return this;
+  }
+
+  public async comparePassword(password: string): Promise<boolean> {
+    return compare(password, this.passwordHash);
   }
 }
