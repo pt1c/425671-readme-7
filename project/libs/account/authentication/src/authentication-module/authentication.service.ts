fd38d0f62a06c4323c0174d8dd2f9582845f4f68
pt1c <kirill@savinov.me>
2024-07-08 13:49:10 +0300

    Добавит метод регистрации в AuthenticationService

--- project/libs/account/authentication/src/authentication-module/authentication.service.ts
+++ project/libs/account/authentication/src/authentication-module/authentication.service.ts
@@ -1,9 +1,34 @@
-import { Injectable } from '@nestjs/common';
-import { BlogUserRepository } from '@project/account/blog-user';
+import { ConflictException, Injectable } from '@nestjs/common';
+import { BlogUserRepository, BlogUserEntity } from '@project/account/blog-user';
+import dayjs from 'dayjs';
+import { CreateUserDto } from '../dto/create-user.dto';
+import { AUTH_USER_EXISTS } from './authentication.constant';
 
 @Injectable()
 export class AuthenticationService {
   constructor(
     private readonly blogUserRepository: BlogUserRepository
   ) {}
+
+  public async register(dto: CreateUserDto) {
+    const {email, username, avatar, password} = dto;
+
+    const blogUser = {
+      email,
+      username,
+      avatar,
+      regDate: dayjs().toDate(),
+      passwordHash: ''
+    };
+
+    const existUser = await this.blogUserRepository.findByEmail(email);
+    if (existUser) {
+      throw new ConflictException(AUTH_USER_EXISTS);
+    }
+
+    const userEntity = await new BlogUserEntity(blogUser).setPassword(password)
+
+    return this.blogUserRepository.save(userEntity);
+  }
+
 }
