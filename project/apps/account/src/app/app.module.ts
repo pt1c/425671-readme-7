daa4bc383861b22efe11ded6837be027ebb583aa
pt1c <kirill@savinov.me>
2024-07-16 07:16:21 +0300

    добавляет работу с mongo

--- project/apps/account/src/app/app.module.ts
+++ project/apps/account/src/app/app.module.ts
@@ -1,14 +1,18 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 
 import { BlogUserModule } from '@project/account/blog-user';
 import { AuthenticationModule } from '@project/account/authentication'
-import { AccountConfigModule } from '@project/account/config';
+import { AccountConfigModule, getMongooseOptions } from '@project/account/config';
 
 @Module({
   imports: [
     BlogUserModule,
     AuthenticationModule,
-    AccountConfigModule
+    AccountConfigModule,
+    MongooseModule.forRootAsync(
+      getMongooseOptions()
+    )
   ],
   controllers: [],
   providers: [],
