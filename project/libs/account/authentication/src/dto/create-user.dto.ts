6c9fc884fc1d94e18e33c5ec00f6286843214066
pt1c <kirill@savinov.me>
2024-07-22 15:22:11 +0300

    добавляет валидацию в аккаунт

--- project/libs/account/authentication/src/dto/create-user.dto.ts
+++ project/libs/account/authentication/src/dto/create-user.dto.ts
@@ -1,27 +1,36 @@
 import { ApiProperty } from '@nestjs/swagger';
+import { IsEmail, IsOptional, IsString } from 'class-validator';
+
+import { AuthenticationValidateMessage } from '../authentication-module/authentication.constant';
+
 
 export class CreateUserDto {
   @ApiProperty({
     description: 'User unique address',
     example: 'user@user.ru'
   })
+  @IsEmail({}, { message: AuthenticationValidateMessage.EmailNotValid })
   public email: string;
 
   @ApiProperty({
     description: 'Username',
     example: 'Djonny',
   })
+  @IsString()
   public username: string;
 
   @ApiProperty({
     description: 'Avatar image',
     example: 'some.png'
   })
+  @IsString()
+  @IsOptional()
   public avatar?: string;
 
   @ApiProperty({
     description: 'User password',
     example: 'FuckYouHacker'
   })
+  @IsString()
   public password: string;
 }
--- project/libs/account/authentication/src/dto/login-user.dto.ts
+++ project/libs/account/authentication/src/dto/login-user.dto.ts
@@ -1,15 +1,21 @@
 import { ApiProperty } from '@nestjs/swagger';
+import { IsEmail } from 'class-validator';
+
+import { AuthenticationValidateMessage } from '../authentication-module/authentication.constant';
+
 
 export class LoginUserDto {
   @ApiProperty({
     description: 'User uniq email',
     example: 'user@user.ru',
   })
+  @IsEmail({}, { message: AuthenticationValidateMessage.EmailNotValid })
   public email: string;
 
   @ApiProperty({
     description: 'User password',
     example: 'FuckYouHacker'
   })
+
   public password: string;
 }
